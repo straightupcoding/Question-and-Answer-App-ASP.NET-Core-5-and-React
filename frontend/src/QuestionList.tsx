c09627f2ea8f16b3b32d788b023fe0ac4582bbdd
greeneterrance92 <straightupcoding@gmail.com>
2021-05-13 17:26:37 -0500

    Adding QuestionList Component
    Adding QuestionList Component

---
+++ frontend/src/QuestionList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { QuestionData } from "./QuestionsData";
+
+interface Props {
+  data: QuestionData[];
+}
+export const QuestionList = (props: Props) => (
+  <ul>
+    {props.data.map((question) => (
+      <li key={question.questionId}></li>
+    ))}
+  </ul>
+);
